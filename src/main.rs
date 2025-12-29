358e308e09c1bda8f80bc0d9516c3ed77f03b38e
Ayush Muley <ayush.muley1907@outlook.com>
2025-12-29 14:25:42 +0000

    Enhanced server to add multi threading functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use little_crab::ThreadPool;
 use std::{
     fs,
     io::{BufReader, prelude::*},
@@ -5,17 +6,20 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:1907");
-    for stream in listener.unwrap().incoming() {
+    let listener = TcpListener::bind("127.0.0.1:1907").unwrap();
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_request(stream);
+        pool.execute(|| {
+            handle_request(stream);
+        });
     }
 }
 
 fn handle_request(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, path) = if request_line == "GET / HTTP 1.1" {
+    let (status_line, path) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "templates/index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "templates/404.html")
